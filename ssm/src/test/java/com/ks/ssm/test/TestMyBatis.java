ae055144586665d2817aed7ab28a5e842b7b7dbd
MichaelChansn <13672025181@163.com>
2016-04-24 22:42:31 +0800

    add test

--- ssm/src/test/java/com/ks/ssm/test/TestMyBatis.java
+++ ssm/src/test/java/com/ks/ssm/test/TestMyBatis.java
@@ -11,7 +11,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.alibaba.fastjson.JSON;
+import com.ks.ssm.dao.ArticleMapper;
+import com.ks.ssm.dao.CommentMapper;
 import com.ks.ssm.dao.UserMapper;
+import com.ks.ssm.domain.Article;
+import com.ks.ssm.domain.Comment;
 import com.ks.ssm.domain.User;
 import com.ks.ssm.service.IUserService;
 
@@ -24,6 +28,10 @@ public class TestMyBatis {
   private IUserService userService = null;
   @Resource
   private UserMapper userDao;
+  @Resource
+  private ArticleMapper articleDao;
+  @Resource
+  private CommentMapper commentDao;
   @Test
   public void test1() {
     User user = userService.getUserById(1);
@@ -39,6 +47,50 @@ public class TestMyBatis {
     for(User userTemp : users)
     {
     	logger.info(JSON.toJSONString(userTemp));
+    }
+  }
+  
+  @Test
+  public void testArticle() {
+    Article article = articleDao.selectByPrimaryKey((long)1);
+    logger.info("article一条记录.........................");
+    logger.info(JSON.toJSONString(article));
+    List<Article> articles=articleDao.selectByUserID((long) 1);
+    logger.info("list多条记录.........................");
+    for(Article articleTemp : articles)
+    {
+    	logger.info(JSON.toJSONString(articleTemp));
+    }
+    List<Article> articles2=articleDao.selectByStatus(false);
+    logger.info("list多条记录.........................");
+    for(Article articleTemp : articles2)
+    {
+    	logger.info(JSON.toJSONString(articleTemp));
+    }
+  }
+  
+  @Test
+  public void testComment() {
+    Comment comment = commentDao.selectByPrimaryKey((long)1);
+    logger.info("comment一条记录.........................");
+    logger.info(JSON.toJSONString(comment));
+    List<Comment> comments1=commentDao.selectByArticleID((long) 1);
+    logger.info("list多条记录.........................");
+    for(Comment commentTemp : comments1)
+    {
+    	logger.info(JSON.toJSONString(commentTemp));
+    }
+    List<Comment> comments=commentDao.selectByUserID((long) 1);
+    logger.info("list多条记录.........................");
+    for(Comment commentTemp : comments)
+    {
+    	logger.info(JSON.toJSONString(commentTemp));
+    }
+    List<Comment> comments2=commentDao.selectByStatus(true);
+    logger.info("list多条记录.........................");
+    for(Comment commentTemp : comments2)
+    {
+    	logger.info(JSON.toJSONString(commentTemp));
     }
   }
 }
